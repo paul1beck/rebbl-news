b937d1d834fee03e74e36d9070a191e395d2bec4
Paul Beck <PaulAlexanderBeck@hotmail.com>
2018-08-12 10:00:26 -0500

    Article CSS Updates, League pagination fix

--- main/routes.py
+++ main/routes.py
@@ -6,7 +6,7 @@ main = Blueprint('main', __name__)
 
 
 @main.route("/")
-@main.route("/home")
+@main.route("/home", methods=['GET'])
 def home():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.filter_by(published=True).order_by(Post.date_posted.desc()).paginate(page=page, per_page=10)
@@ -20,7 +20,7 @@ def about():
 def unpublished():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.filter_by(published=False).order_by(Post.date_posted.desc()).paginate(page=page, per_page=10)
-    return render_template('unpublished.html', posts=posts)
+    return render_template('home.html', posts=posts)
 
 @main.route("/clan")
 def clan():
@@ -28,7 +28,7 @@ def clan():
     posts = Post.query.filter(Post.category.in_(["Clan","All"])).filter_by(published=True).order_by(Post.date_posted.desc()).paginate(page=page, per_page=10)
     return render_template('home.html', posts=posts)
 
-@main.route("/clan/only")
+@main.route("/clan/only", methods=['GET'])
 def clan_only():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.filter_by(category="Clan", published=True).order_by(Post.date_posted.desc()).paginate(page=page, per_page=10)
@@ -38,34 +38,34 @@ def clan_only():
 def rel():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.filter(Post.category.in_(["REL","All"])).filter_by(published=True).order_by(Post.date_posted.desc()).paginate(page=page, per_page=10)
-    return render_template('home.html', posts=posts)
+    return render_template('rel.html', posts=posts)
 
-@main.route("/rel/only")
+@main.route("/rel/only", methods=['GET'])
 def rel_only():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.filter_by(category="REL", published=True).order_by(Post.date_posted.desc()).paginate(page=page, per_page=10)
-    return render_template('home.html', posts=posts)
+    return render_template('rel.html', posts=posts)
 
 @main.route("/gman")
 def gman():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.filter(Post.category.in_(["GMAN","All"])).filter_by(published=True).order_by(Post.date_posted.desc()).paginate(page=page, per_page=10)
-    return render_template('home.html', posts=posts)
+    return render_template('gman.html', posts=posts)
 
-@main.route("/gman/only")
+@main.route("/gman/only", methods=['GET'])
 def gman_only():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.filter_by(category="GMAN", published=True).order_by(Post.date_posted.desc()).paginate(page=page, per_page=10)
-    return render_template('home.html', posts=posts)
+    return render_template('gman.html', posts=posts)
 
 @main.route("/big-o")
 def bigo():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.filter(Post.category.in_(["Big O","All"])).filter_by(published=True).order_by(Post.date_posted.desc()).paginate(page=page, per_page=10)
-    return render_template('home.html', posts=posts)
+    return render_template('bigo.html', posts=posts)
 
-@main.route("/big-o/only")
+@main.route("/big-o/only", methods=['GET'])
 def bigo_only():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.filter_by(category="Big O", published=True).order_by(Post.date_posted.desc()).paginate(page=page, per_page=10)
-    return render_template('home.html', posts=posts)
+    return render_template('bigo.html', posts=posts)
